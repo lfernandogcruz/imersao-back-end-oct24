0bd627ca56c7b0aa0231ce4a9e83f2912dcb654f
LuisCruz <lfernandogcruz@yahoo.com.br>
2024-11-08 18:54:33 -0300

    aula 2 e 3

--- introdutorio/app.js
+++ introdutorio/app.js
@@ -1,10 +1,12 @@
 alert('Boas vindas ao jogo do Número secreto!');
 
-let numeroSecreto = 29;
+let numeroSecreto = 5;
 
-let chute = prompt('Escolha um número entre 1 e 30');
+let chute = prompt('Escolha um número entre 1 e 10');
 
 if (chute == numeroSecreto) {
-  console.log('você descobriu o numero secreto');
+  alert(`você descobriu o numero secreto (${numeroSecreto})`);
   
-}
+} else {
+  alert('Voce errou');
+};

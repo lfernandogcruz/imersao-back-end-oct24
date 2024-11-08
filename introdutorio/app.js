82ee1802079308f9bfb3b3421a323bfa518327da
LuisCruz <lfernandogcruz@yahoo.com.br>
2024-11-08 17:36:20 -0300

    first commit

---
+++ introdutorio/app.js
@@ -0,0 +1,10 @@
+alert('Boas vindas ao jogo do Número secreto!');
+
+let numeroSecreto = 29;
+
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if (chute == numeroSecreto) {
+  console.log('você descobriu o numero secreto');
+  
+}

6d87532055269288365d3bccd5bf290246a494c2
LuisCruz <lfernandogcruz@yahoo.com.br>
2024-11-10 00:48:12 -0300

    desafio 09-02 - POSITIVO/NEGATIVO

---
+++ introdutorio/desafios/09-02.js
@@ -0,0 +1,10 @@
+let numero = prompt('Digite um número:');
+
+if (numero > 0) {
+  alert(`Seu número é POSITIVO! (${numero})`);
+} else if (numero < 0) {
+  alert(`Seu número é NEGATIVO! (${numero})`);
+} else {
+  alert(`Seu número é NEUTRO! (${numero})`);
+};
+ 

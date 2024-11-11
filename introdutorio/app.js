18a85c55981f6ca2b616d814b9bb75c26b1b0804
LuisCruz <lfernandogcruz@yahoo.com.br>
2024-11-10 23:50:55 -0300

    adicao do loop while para refazer tentativas até o acerto do num secreto

--- introdutorio/app.js
+++ introdutorio/app.js
@@ -1,16 +1,20 @@
 alert('Boas vindas ao jogo do Número secreto!');
 
 let numeroSecreto = 5;
+let chute;
 
-let chute = prompt('Escolha um número entre 1 e 10');
+while (chute != numeroSecreto) {
+  chute = prompt('Escolha um número entre 1 e 10');
 
-if (chute == numeroSecreto) {
-  alert(`você descobriu o numero secreto (${numeroSecreto})`);
-  
-} else {
-  if (chute > numeroSecreto) {
-    alert(`O número secreto é menor que ${chute}`)
+  if (chute == numeroSecreto) {
+    alert(`você descobriu o numero secreto (${numeroSecreto})`);
+    
   } else {
-    alert(`O número secreto é maior que ${chute}`)
-  }
-};
+    if (chute > numeroSecreto) {
+      alert(`O número secreto é menor que ${chute}`)
+    } else {
+      alert(`O número secreto é maior que ${chute}`)
+    }
+  };
+}
+
